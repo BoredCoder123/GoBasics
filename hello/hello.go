612d771ab13b324b7bb549d30b155ce8e1e3ff30
Ankit Kathait <ankitkathait28@gmail.com>
2023-12-26 20:13:39 +0530

    Maps

--- hello/hello.go
+++ hello/hello.go
@@ -9,11 +9,20 @@ func main ()  {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Ankit")
-	if err!= nil {
-        log.Fatal(err)
-    }
+	// message, err := greetings.Hello("Ankit")
+	// if err!= nil {
+    //     log.Fatal(err)
+    // }
 
-	fmt.Println(message)
-	
+	// fmt.Println(message)
+
+	names := []string{"Ankit", "Kattu", "Kathait"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for key, val := range messages {
+		fmt.Println(key + ": " + val)
+	}
 }
